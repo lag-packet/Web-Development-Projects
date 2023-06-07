cd8d899059b81dd980b149102e17be889fa82986
lag-packet <satarhassni0@gmail.com>
2023-06-07 17:59:19 -0400

    added memory game

---
+++ Native/memory-game/app.js
@@ -0,0 +1,92 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png',
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png',
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    },
+];
+
+cardArray.sort(() => 0.5 - Math.random());
+
+const gridDisplay = document.querySelector('#grid');
+const cardsChosen = [];
+const cardsChosenIds = [];
+
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img');
+        card.setAttribute('src', 'images/blank.png');
+        card.setAttribute("data-id", i);
+        card.addEventListener('click', flipCard);
+        gridDisplay.append(card);
+    }
+}
+
+createBoard();
+
+function checkMatch() {
+    const cards = document.querySelectorAll('#grid img');
+    console.log(cards);
+    console.log('check for match!');
+    if (cardsChosen[0] == cardsChosen[1]) {
+        alert('found a match');
+        cards[cardsChosenIds[0]].setAttribute('src', 'images/white.png');
+        cards[cardsChosenIds[1]].setAttribute('src', 'images/white.png');
+    }
+
+}
+
+function flipCard() {
+    let cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenIds.push(cardId);
+    console.log(cardsChosen);
+    console.log(cardsChosenIds);
+    this.setAttribute('src', cardArray[cardId].img);
+    if (cardsChosen.length === 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
